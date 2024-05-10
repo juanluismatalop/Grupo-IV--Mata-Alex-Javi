58669c8390d857e63449636e10a0e5fa5d068783
Juan Luis Mata <juanluismatalop@gmail.com>
2024-05-10 09:22:18 +0200

    pruba registro

--- src/main/java/org/example/demo/HelloApplication.java
+++ src/main/java/org/example/demo/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("logInPrueba.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 450);
         stage.setTitle("Hello!");
         stage.setScene(scene);
--- src/main/java/org/example/demo/LogInPrueba.java
+++ src/main/java/org/example/demo/LogInPrueba.java
@@ -1,4 +1,43 @@
 package org.example.demo;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LogInPrueba {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    @FXML
+    private TextField textPassword, textLogin;
+    @FXML
+    private Label labelUserError;
+    @FXML
+    private Button buttonSubmit;
+
+    @FXML
+    public void onClick(ActionEvent actionEvent) throws IOException {
+        System.out.println("pulsado boton");
+        String login = textLogin.getText();
+        String password = textPassword.getText();
+        System.out.println(login + "--" + password);
+        if (login.equals("manuel") && password.equals("1234")) {
+            System.out.println("Cambiamos de ventana");
+            stage = (Stage) buttonSubmit.getScene().getWindow();
+            root = FXMLLoader.load(getClass().getResource("ventana-view.fxml"));
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
+        else
+            labelUserError.setText("Usuario incorrecto");
+    }
 }
