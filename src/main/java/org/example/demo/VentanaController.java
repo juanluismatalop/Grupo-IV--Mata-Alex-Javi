7a9edc5bc78598a7d2243b4a77ff177af5a08ba9
julu <juanluismatalop@gmail.com>
2024-05-17 09:11:56 +0100

    creacion de ventana

--- src/main/java/org/example/demo/VentanaController.java
+++ src/main/java/org/example/demo/VentanaController.java
@@ -1,4 +1,27 @@
 package org.example.demo;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class VentanaController {
+    public Button buttonCerrarSesion;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    @FXML
+    public void buttonCerrarSesion(ActionEvent actionEvent) throws IOException {
+        stage = (Stage) buttonCerrarSesion.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("logInPrueba.fxml"));
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
