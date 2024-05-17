6f92fa8506da40af13383dc7f0ef073f8141476c
julu <juanluismatalop@gmail.com>
2024-05-17 13:49:58 +0100

    creacion ventanas y enlazarla con la principal

--- src/main/java/org/example/demo/VentanaController.java
+++ src/main/java/org/example/demo/VentanaController.java
@@ -12,6 +12,9 @@ import java.io.IOException;
 
 public class VentanaController {
     public Button buttonCerrarSesion;
+    public Button buttonHoteles;
+    public Button buttonReservas;
+    public Button buttonApartamento;
     private Stage stage;
     private Scene scene;
     private Parent root;
@@ -20,6 +23,33 @@ public class VentanaController {
     public void buttonCerrarSesion(ActionEvent actionEvent) throws IOException {
         stage = (Stage) buttonCerrarSesion.getScene().getWindow();
         root = FXMLLoader.load(getClass().getResource("logInPrueba.fxml"));
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        System.out.println("Boton pulsado");
+    }
+    @FXML
+    public void buttonHoteles(ActionEvent actionEvent) throws IOException {
+        stage = (Stage) buttonHoteles.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("Hoteles-view.fxml"));
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        System.out.println("Boton pulsado");
+    }
+    @FXML
+    public void buttonReservas(ActionEvent actionEvent) throws IOException {
+        stage = (Stage) buttonReservas.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("Reservas-view.fxml"));
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        System.out.println("Boton pulsado");
+    }
+    @FXML
+    public void buttonApartamento(ActionEvent actionEvent) throws IOException {
+        stage = (Stage) buttonApartamento.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("ApartamentosTuristicos-view.fxml"));
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
