e3ad090b0906ebbd75bfb9d074b754551c3aa080
julu <juanluismatalop@gmail.com>
2024-05-20 17:01:13 +0100

    cambios 20/05/24

--- src/main/java/org/example/demo/HotelesView.java
+++ src/main/java/org/example/demo/HotelesView.java
@@ -12,11 +12,19 @@ import java.io.IOException;
 public class HotelesView {
 
     public Button backButton;
+    public Button buttonAdd;
     private Stage stage;
     private Parent root;
     private Scene scene;
 
     public void back(ActionEvent actionEvent) throws IOException {
+        stage = (Stage) backButton.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("ventana-view.fxml"));
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void buttonAdd(ActionEvent actionEvent) throws IOException {
         stage = (Stage) backButton.getScene().getWindow();
         root = FXMLLoader.load(getClass().getResource("ventana-view.fxml"));
         scene = new Scene(root);
