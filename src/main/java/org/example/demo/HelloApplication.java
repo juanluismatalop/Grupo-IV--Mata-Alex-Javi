d95b310a4948213dd6236d07ad24b42c30d561ee
julu <juanluismatalop@gmail.com>
2024-05-22 09:51:58 +0100

    commit cambios 22/05/24

--- src/main/java/org/example/demo/HelloApplication.java
+++ src/main/java/org/example/demo/HelloApplication.java
@@ -13,7 +13,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("logInPrueba.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 450);
-        stage.setTitle("Hello!");
+        stage.setTitle("Hoteles Triana");
         stage.setScene(scene);
         stage.show();
     }
