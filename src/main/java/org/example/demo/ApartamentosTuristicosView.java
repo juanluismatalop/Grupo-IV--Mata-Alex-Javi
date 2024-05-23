56f27022bd319e55747cf1ed716e3116edd0e705
JaviSego1 <javiersegoviamartinez@gmail.com>
2024-05-23 12:40:56 +0200

    COMMIT VENTANA APARTAMENTOS

---
+++ src/main/java/org/example/demo/ApartamentosTuristicosView.java
@@ -0,0 +1,35 @@
+package org.example.demo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Dialog;
+import javafx.stage.Stage;
+import javafx.util.Pair;
+
+import java.io.IOException;
+
+public class ApartamentosTuristicosView {
+
+    public Button backButton;
+    public Button buttonAdd;
+    private Stage stage;
+    private Parent root;
+    private Scene scene;
+
+    public void back(ActionEvent actionEvent) throws IOException {
+        stage = (Stage) backButton.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("ventana-view.fxml"));
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void buttonAdd(ActionEvent actionEvent){
+        Dialog<Pair<String, String>> dialog = new Dialog<>();
+        dialog.setTitle("Añadir Apartamento");
+        dialog.setHeaderText("Añadir Apartamento");
+    }
+}
